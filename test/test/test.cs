10681b7ae017b29a4b668987243caf7ac7bee1e6
carlos <guzmanc1@nku.edu>
2017-11-12 08:41:30 -0500

    first commit

---
+++ test/test/test.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+
+namespace test
+{
+    [TestFixture]
+    public class MainClassTest
+    {
+        [Test]
+        public void DrawGrid()
+        {
+            MainClass source = new MainClass();
+            string puzzle = "";
+            string answer = "";
+
+            Assert.AreEqual(150, 100);
+        }
+    }
+}
